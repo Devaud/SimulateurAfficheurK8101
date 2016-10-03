7978140f2eb93fa909beb337c8a43ac18958fcb0
Devaud <devaud.alan@gmail.com>
2016-10-03 09:37:25 +0200

    Modification :
        - Class SimulatorLCD, ajout de l'affichage qui change de couleur

--- SimulateurAfficheurVellemanK8101/SimulateurAfficheurVellemanK8101/SimulatorLCD.cs
+++ SimulateurAfficheurVellemanK8101/SimulateurAfficheurVellemanK8101/SimulatorLCD.cs
@@ -13,20 +13,29 @@ namespace SimulateurAfficheurVellemanK8101
         #region const
         // It's constant but can't be declared constant
         private static Point DEFAULT_LOCATION = new Point(0, 0);
-        private static Size DEFAULT_SIZE = new Size(150, 100);
+        private static Size DEFAULT_SIZE = new Size(150, 90);
         private static Color DEFAULT_PEN_COLOR = Color.Black;
         private static Color DEFAULT_BRUSH_COLOR = Color.White;
 
-        private const float DEFAULT_PEN_WIDTH = 1f; 
+        private const float DEFAULT_PEN_WIDTH = 1f;
+        private const int ADAPTIF_RATER = 5;
         #endregion
         
         #region Fields
         private Rectangle _lcd;
+        private Rectangle _backColorLcd;
         private Pen _pen;
         private Brush _brush;
+        private Brush _brushBackColor;
         #endregion
 
         #region Properties
+        public Brush BrushBackColor
+        {
+            get { return _brushBackColor; }
+            set { _brushBackColor = value; }
+        }
+
         public Rectangle Lcd
         {
             get { return _lcd; }
@@ -44,6 +53,12 @@ namespace SimulateurAfficheurVellemanK8101
             get { return _brush; }
             set { _brush = value; }
         }
+
+        public Rectangle BackColorLcd
+        {
+            get { return _backColorLcd; }
+            set { _backColorLcd = value; }
+        }
         #endregion
 
         #region Constructor
@@ -59,15 +74,19 @@ namespace SimulateurAfficheurVellemanK8101
 
         public SimulatorLCD(Point location, Size size)
         {
-            this.Lcd = new Rectangle(location, size);
+            this.BackColorLcd = new Rectangle(location, size);
+            this.Lcd = new Rectangle(new Point(location.X + ADAPTIF_RATER, location.Y + ADAPTIF_RATER), new Size(size.Width - ADAPTIF_RATER * 2, size.Height - ADAPTIF_RATER * 2));
             this.Pen = new Pen(DEFAULT_PEN_COLOR, DEFAULT_PEN_WIDTH);
             this.Brush = new SolidBrush(DEFAULT_BRUSH_COLOR);
+            this.BrushBackColor = new SolidBrush(DEFAULT_BRUSH_COLOR);
         }
         #endregion
 
         #region Methods
         public void Draw(PaintEventArgs pe)
         {
+            pe.Graphics.FillRectangle(this.BrushBackColor, this.BackColorLcd);
+            pe.Graphics.DrawRectangle(this.Pen, this.BackColorLcd);
             pe.Graphics.FillRectangle(this.Brush, this.Lcd);
             pe.Graphics.DrawRectangle(this.Pen, this.Lcd);
         }
