afd2995953431ff85dd8fd1df0064a304d04f32b
Devaud <devaud.alan@gmail.com>
2016-11-07 11:24:26 +0100

    Add the project for the good orientation

---
+++ SimuK8101/SimuK8101/SimuK8101.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimuK8101
+{
+    public class SimuK8101
+    {
+        public SimuK8101()
+        {
+
+        }
+    }
+}
---
+++ SimuK8101/TestProgram/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Velleman.Kits;
+
+namespace TestProgram
+{
+    public partial class Form1 : Form
+    {
+        K8101 display;
+
+        public K8101 Display
+        {
+            get { return display; }
+            set { display = value; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            Display = new K8101();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Display.Connect();
+            } 
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
